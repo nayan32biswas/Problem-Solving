97d0a4c8c51140eb91d6606183767adb88c0f510
Nayan Biswas <nayan32biswas@gmail.com>
2021-12-03 14:12:19 +0600

    second solution of day-3

--- adventofcode2021/day-3.py
+++ adventofcode2021/day-3.py
@@ -4,18 +4,11 @@ inputs = inp.readlines()
 inputs = [data.strip() for data in inputs]
 
 
-def max(zero, one):
-    if zero > one:
-        return 0
-    else:
-        return 1
-
-
 def bool2int(arr):
     arr = reversed(arr)
     num = 0
     for i, j in enumerate(arr):
-        num += j << i
+        num += int(j) << i
     return num
 
 
@@ -29,7 +22,7 @@ def first(inputs):
                 zeros[i] += 1
             else:
                 ones[i] += 1
-    ans = [max(zeros[i], ones[i]) for i in range(arr_size)]
+    ans = [1 if zeros[i] < ones[i] else 0 for i in range(arr_size)]
     gama = bool2int(ans)
     ans = [0 if val == 1 else 1 for val in ans]
     epsilon = bool2int(ans)
@@ -37,4 +30,43 @@ def first(inputs):
     return gama * epsilon
 
 
-print(first(inputs))
+def filter_inputs(inputs, pos, com_val):
+    return [val for val in inputs if val[pos] == com_val]
+
+
+def find_value(inputs, com_val):
+    input_for_oxigen = inputs
+    line_size = len(inputs[0])
+    for i in range(line_size):
+        zero, one = 0, 0
+        if len(input_for_oxigen) == 1:
+            print(i)
+            break
+        for val in input_for_oxigen:
+            if val[i] == "0":
+                zero += 1
+            elif val[i] == "1":
+                one += 1
+        if zero > one:
+            if(com_val == "1"):
+                input_for_oxigen = filter_inputs(input_for_oxigen, i, "0")
+            else:
+                input_for_oxigen = filter_inputs(input_for_oxigen, i, "1")
+        elif one > zero:
+            if(com_val == "1"):
+                input_for_oxigen = filter_inputs(input_for_oxigen, i, "1")
+            else:
+                input_for_oxigen = filter_inputs(input_for_oxigen, i, "0")
+        else:
+            input_for_oxigen = filter_inputs(input_for_oxigen, i, com_val)
+    return bool2int(input_for_oxigen[0])
+
+
+def second(inputs):
+    oxigen = find_value(inputs, "1")
+    co2 = find_value(inputs, "0")
+    return oxigen * co2
+
+
+# print(first(inputs))
+print(second(inputs))
